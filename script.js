798cc7837a8ff8c5767a9ff7d01fecea07b71aa6
Z125093-paillereau-thomas <z125093@shibaura-it.ac.jp>
2025-05-15 05:10:09 +0900

    added everything

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function showSideBar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function hideSideBar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}

e47d237973a35e80f89fcc7ced330bb2de4d7de9
Z125093-paillereau-thomas <z125093@shibaura-it.ac.jp>
2025-05-23 11:21:24 +0900

    added correct Exp and diplomas picture

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function hideSideBar()
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
+
+function expDisplay(n)
+{
+    const textBox = document.getElementById("expTextBox");
+    let a = textBox.children;
+    for (let el of a)
+    {
+        el.style.display = "none";
+    }
+    a[n].style.display = "block";
+}

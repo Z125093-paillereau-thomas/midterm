b9fb8323164360c553494e3a1768d11cb0d457f7
Z125093-paillereau-thomas <z125093@shibaura-it.ac.jp>
2025-05-23 15:26:16 +0900

    added good function of the diplomas + little main frame animation

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
 "use strict";
 
+/// Variables
+
+// Nav
+const sidebar = document.querySelector(".sidebar");
+
+// Mainframe
+const mainFrame = document.querySelector(".mainFrame").querySelector(".container").querySelector("img");
+
+//Diplomas
+const diplomas = document.getElementById("diplomas");
+const dTimeline = diplomas.querySelector(".timeline");
+const dContentList = dTimeline.querySelectorAll(".content");
+
+// Experience
+const textBox = document.getElementById("expTextBox");
+const expTexts = textBox.children;
+
+// Projects
+
+
+
+
+
+/// Functions
+
+// Nav
 function showSideBar()
 {
-    const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "flex";
 }
 
 function hideSideBar()
 {
-    const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
 
+//Mainframe
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    mainFrame.style.transform = `translateY(${-scrollY * 0.5}px)`;
+})
+
+
+
+
+// Diplomas
+
+function dipDisplay(n)
+{
+    let d = dContentList[n];
+    let t = d.querySelector(".text-box");
+    if (t.style.display === "flex")
+        t.style.display = "none";
+    else
+        t.style.display = "flex";
+}
+
+
+// Experience
 function expDisplay(n)
 {
-    const textBox = document.getElementById("expTextBox");
-    let a = textBox.children;
-    for (let el of a)
+    for (let el of expTexts)
     {
         el.style.display = "none";
     }
-    a[n].style.display = "block";
+    expTexts[n].style.display = "block";
 }
+
